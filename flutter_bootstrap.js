f024073b1a7fb7b03e7b6cd4b88ad0b087c11124
ismayilac123 <ackismayil123@gmail.com>
2026-03-31 13:50:00 +0530

    Update Flutter web build

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"a5cb96369ef86c7e85abf5d662a1ca5d897750
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2965635503"
+    serviceWorkerVersion: "2192163177"
   }
 });
